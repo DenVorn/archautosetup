d53115a1cfeecec42bbeffd572a32967f16d9628
Vorn <100448229+DenVorn@users.noreply.github.com>
2022-05-16 17:57:13 +0200

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,6 +1,4 @@
-
-
-#phase 1
+#Configures the settings
 ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 hwclock --systohc
 echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
@@ -9,13 +7,10 @@ echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 echo "KEYMAP=se-latin1" > /etc/vconsole.conf
 echo "127.0.0.1 localhost" > /etc/hostname
 
-#SCript stops execution after mkinitcpio
+#Compiles recent changes
 mkinitcpio -P
 
-
-
-#phase 2
-
+#Installs shit I'll be using
 yes | pacman -Syu
 yes | pacman -S nano
 yes | pacman -S git
@@ -24,33 +19,33 @@ yes | pacman -S iwd
 systemctl enable iwd
 yes | pacman -S dhcpcd
 yes | pacman -S sudo
+yes | pacman -S firefox
+yes | pacman -S discord
 
+#Creates account, changes passwords and configures sudoers
 useradd -m voron
 echo "voron:3947" | chpasswd
 echo "root:3947" | chpasswd
-
 echo "Voron ALL=(ALL:ALL) ALL" > /etc/sudoers
 echo "Defaults:Voron	!authenticate" > /etc/sudoers
 
+#Installs and configures grub bootloader
 yes | pacman -S grub
 yes | pacman -S efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+#Installs and configures the graphics and Desktop enviroment
 printf "\n y\n" | pacman -S xorg
-
 yes | pacman -S nvidia
-
 Xorg :0 -configure
 printf "\n\n\n\n\n y\n" | pacman -S plasma
-
 systemctl set-default graphical.target
 systemctl enable sddm
-
 setxkbmap sv
 
-yes | pacman -S firefox
-yes | pacman -S discord
-
-
+#Testing purposes only
 #systemctl enalbe dhcpcd@ens33
+
+#Reboots
+reboot
