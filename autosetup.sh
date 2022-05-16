23961d68e2f4996019c03ee3341556ed68f69b4c
Vorn <100448229+DenVorn@users.noreply.github.com>
2022-05-16 17:57:34 +0200

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -1,22 +1,27 @@
+#Prob don't need this
 loadkeys sv-latin1
 timedatectl set-ntp true
 
+#Creates the cluster
 printf "g\n n\n\n\n +1G\n t\n 1\n w\n" | fdisk /dev/sda
 printf "n\n\n\n +16G\n t\n\n 19\n w\n" | fdisk /dev/sda
 printf "n\n\n\n\n t\n\n 23\n w\n" | fdisk /dev/sda
 
+#Formats the cluster
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 
+#Mounts the cluster
 mount /dev/sda3 /mnt
 swapon /dev/sda2
 mount --mkdir /dev/sda1 /mnt/boot
 
-
+#Installs linux kernel
 pacstrap /mnt base linux-lts linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
+#Starts next script
 chmod +x pack.sh
 cp pack.sh /mnt
 arch-chroot /mnt ./pack.sh
